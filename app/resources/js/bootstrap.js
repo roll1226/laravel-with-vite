9a6f2aad43a106ab4648ae4f910a6bde5cd0f4ef
roll1226 <tomo_yasu_4724@icloud.com>
2024-04-08 08:02:27 +0900

    vue

--- app/resources/js/bootstrap.js
+++ app/resources/js/bootstrap.js
@@ -4,10 +4,12 @@
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-import axios from 'axios';
-window.axios = axios;
+import axios from "axios";
+import { createApp } from "vue/dist/vue.esm-bundler.js";
+import App from "./vue/App.vue";
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios = axios;
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
@@ -30,3 +32,11 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
 //     enabledTransports: ['ws', 'wss'],
 // });
+
+const app = createApp({
+    components: {
+        App,
+    },
+});
+
+app.mount("#app");
--- app/vite.config.js
+++ app/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     server: {
@@ -10,6 +11,14 @@ export default defineConfig({
         },
     },
     plugins: [
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
