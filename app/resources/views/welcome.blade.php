9eed021f2faa8195158674696e5ccdaa9bf564a8
roll1226 <tomo_yasu_4724@icloud.com>
2024-04-12 07:35:54 +0900

    add: string laravel to vue

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -834,7 +834,7 @@
 
 <body class="antialiased" id='app'>
     <app></app>
-    <task-component context="context"></task-component>
+    <task-component context={{ $context }}></task-component>
     <div
         class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
         @if (Route::has('login'))
--- app/routes/web.php
+++ app/routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $context = 'hugaaaaa';
+    return view('welcome', compact("context"));
 });
