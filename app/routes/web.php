343e55032a98c1532906fc5890a677806b436657
roll1226 <tomo_yasu_4724@icloud.com>
2024-07-09 06:55:50 +0900

    add: Auth

--- app/routes/web.php
+++ app/routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 });
 
 Route::resource('/tasks', TaskController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
